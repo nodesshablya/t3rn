e564eee446efe9dcbb275366a28fc224230d3b15
SHABLYA <117276220+nodesshablya@users.noreply.github.com>
2025-01-24 22:29:04 +0200

    Update t3rn_installer.sh

--- t3rn_installer.sh
+++ t3rn_installer.sh
@@ -47,13 +47,13 @@ log_info "Создаем директорию для установки..."
 mkdir t3rn
 cd t3rn || { log_error "Не удалось перейти в директорию t3rn."; exit 1; }
 
-log_info "Скачиваем релиз версии v0.46.0..."
-wget https://github.com/t3rn/executor-release/releases/download/v0.46.0/executor-linux-v0.46.0.tar.gz || {
-  log_error "Не удалось скачать релиз версии v0.46.0."; exit 1;
+log_info "Скачиваем релиз версии v0.47.0..."
+wget https://github.com/t3rn/executor-release/releases/download/v0.47.0/executor-linux-v0.47.0.tar.gz || {
+  log_error "Не удалось скачать релиз версии v0.47.0."; exit 1;
 }
 
 log_info "Распаковываем архив..."
-tar -xzf executor-linux-v0.46.0.tar.gz || { log_error "Ошибка при распаковке архива."; exit 1; }
+tar -xzf executor-linux-v0.47.0.tar.gz || { log_error "Ошибка при распаковке архива."; exit 1; }
 
 log_info "Переходим в директорию бинарных файлов..."
 cd executor/executor/bin || { log_error "Не удалось перейти в директорию бинарных файлов."; exit 1; }
@@ -111,7 +111,6 @@ Environment="EXECUTOR_PROCESS_PENDING_ORDERS_FROM_API=false"
 Environment="EXECUTOR_MAX_L3_GAS_PRICE=850"
 Environment="EXECUTOR_ENABLE_BATCH_BIDDING=false"
 Environment="PRIVATE_KEY_LOCAL=$PRIVATE_KEY_LOCAL"
-Environment="ENABLED_NETWORKS=arbitrum-sepolia,base-sepolia,blast-sepolia,optimism-sepolia,l1rn"
 Environment="RPC_ENDPOINTS_ARBT=$RPC_ARBITRUM"
 Environment="RPC_ENDPOINTS_BSSP=$RPC_BASE"
 Environment="RPC_ENDPOINTS_BLSS=$RPC_BLAST"
